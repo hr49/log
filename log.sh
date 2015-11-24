2dab91b4c94e1500359352f858cf8b409d524b9a
Matthew <matthew.marting@earthlink.net>
2015-11-23 22:56:31 -0500

    returns 's return if install returns zero

--- log.sh
+++ log.sh
@@ -4,3 +4,4 @@ directory="$HOME/log/$1"
 test -d "$directory" && { index="$(\ls "$directory" | grep '^[1-9] [1-9][0-9]\{0,8\} \S.*' | sed 's/^[1-9] \([1-9][0-9]\{0,8\}\) \S.*/\1/g' | sort -n | tail -n1; exit "${PIPESTATUS[0]}")" || exit 1; }
 let index+=1
 "$@" | install -D /dev/stdin "$directory/${#index} $index ${*:2}" || exit 1
+exit "${PIPESTATUS[0]}"
