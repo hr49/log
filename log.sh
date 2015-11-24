5c37ecaa767835ae1f7cebd9b0142771a1237370
Matthew <matthew.marting@earthlink.net>
2015-11-24 08:17:25 -0500

    writes 1 and 2 to /dev/tty

--- log.sh
+++ log.sh
@@ -3,5 +3,6 @@ unalias -a
 directory="$HOME/log/$1"
 test -d "$directory" && { index="$(\ls "$directory" | grep '^[1-9] [1-9][0-9]\{0,8\} \S.*' | sed 's/^[1-9] \([1-9][0-9]\{0,8\}\) \S.*/\1/g' | sort -n | tail -n1; exit "${PIPESTATUS[0]}")" || exit 1; }
 let index+=1
-"$@" | install -D /dev/stdin "$directory/${#index} $index ${*:2}" || exit 1
+directory="$directory/${#index} $index ${*:2}"
+{ "$@" | tee /dev/tty | install -D /dev/stdin "$directory/1" || exit 1; } 2>&1 | tee /dev/tty | install -D /dev/stdin "$directory/2" || exit 1
 exit "${PIPESTATUS[0]}"
