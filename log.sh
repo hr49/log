17d5fb6c19f0f7b421a425703d156adcc267083c
Matthew <matthew.marting@earthlink.net>
2015-11-24 23:30:36 -0500

    fixes multiple EXIT trap

--- log.sh
+++ log.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 set -eo pipefail
 unalias -a
-d="$HOME/log/$1"
-if test -d "$d"; then
-        index=$(ls "$d" | sed 's/^[1-9] \([1-9][0-9]\{0,8\}\)\( \S\+\)\+$/\1/' | sort -n | tail -n1)
+log_directory_directory="$HOME/log/$1"
+if [[ -d $log_directory_directory ]]; then
+        log_index=$(ls "$log_directory_directory" | sed 's/^[1-9] \([1-9][0-9]\{0,8\}\)\( \S\+\)\+$/\1/' | sort -n | tail -n1)
 fi
-((++index))
-d="$d/${#index} $index ${*:2}"
-tmp_d=$(mktemp -d)
-fifo=$tmp_d/3
-mkfifo $fifo
-trap "rm -rf $tmp_d" 0
-exec 3<> $fifo
-trap "exec 3>&-" 0
-"$@" 2>&3 | tee /dev/tty | install -D /dev/stdin "$d/1" | 3>&1 tee /dev/tty | install -D /dev/stdin "$d/2"
+((++log_index))
+log_directory="$log_directory_directory/${#log_index} $log_index ${*:2}"
+error_fifo_directory="$(mktemp -d)"
+trap "rm -fr $error_fifo_directory" EXIT
+error_fifo=$error_fifo_directory/error
+mkfifo $error_fifo
+exec 3<> $error_fifo
+
+install -d "$log_directory"
+chmod 000 "$log_directory"
+
+"$@" 2>&3 | tee /dev/tty | install -D /dev/stdin "$log_directory/1" | 3>&1 tee /dev/tty | install -D /dev/stdin "$log_directory/2"
