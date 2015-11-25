0409af1d5206c4ab50072d4e63a521b7da49463e
matthew <m5w@users.noreply.github.com>
2015-11-25 17:30:17 -0500

    removes log index length and uses $@ instead of $*

--- log.sh
+++ log.sh
@@ -3,13 +3,13 @@ set -eo pipefail
 unalias -a
 log_directory_directory="$HOME/log/$1"
 if [[ -d $log_directory_directory ]]; then
-        log_index=$(ls "$log_directory_directory" | sed 's/^[1-9] \([1-9][0-9]\{0,8\}\)\( \S\+\)\+$/\1/' | sort -n | tail -n1)
+        log_index=$(ls "$log_directory_directory" | sed 's/^\([1-9][0-9]\+\)\( \S\+\)\+$/\1/' | sort -n | tail -n1)
 fi
 ((++log_index))
-log_directory="$log_directory_directory/${#log_index} $log_index ${*:2}"
+log_directory="$log_directory_directory/$log_index ${@:2}"
 error_fifo_directory="$(mktemp -d)"
 trap "rm -fr $error_fifo_directory" EXIT
-error_fifo=$error_fifo_directory/error
+error_fifo=$error_fifo_directory/log.sh_error_fifo
 mkfifo $error_fifo
 exec 3<> $error_fifo
 "$@" 2>&3 | tee /dev/tty | install -D /dev/stdin "$log_directory/1" | 3>&1 tee /dev/tty | install -D /dev/stdin "$log_directory/2"
