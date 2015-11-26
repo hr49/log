804882f0f081452c8483b4e885d3ff5d628cd613
matthew <m5w@users.noreply.github.com>
2015-11-25 21:03:45 -0500

    fixes trailing log directory spaces by checking if the command arguments are empty

--- log.sh
+++ log.sh
@@ -6,7 +6,10 @@ if [[ -d $log_directory_directory ]]; then
         log_index=$(ls "$log_directory_directory" | sed 's/^\([1-9][0-9]*\)\( \S\+\)\+$/\1/' | sort -n | tail -n1)
 fi
 ((++log_index))
-log_directory="$log_directory_directory/$log_index ${@:2}"
+log_directory="$log_directory_directory/$log_index"
+if [[ ! -z ${@:2} ]]; then
+        log_directory="$log_directory ${@:2}"
+fi
 error_fifo_directory="$(mktemp -d)"
 trap "rm -fr $error_fifo_directory" EXIT
 error_fifo=$error_fifo_directory/log.sh_error_fifo
