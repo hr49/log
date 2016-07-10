687787e086a7462a1d1cf5779916c920d6adb89f
matthew <m5w@users.noreply.github.com>
2016-07-10 16:03:58 -0400

    stdout and -err to their respective streams

--- log.sh
+++ log.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 set -eo pipefail
 unalias -a
-if [[ -z $1 ]]; then
-        exit 1
-fi
-a="$HOME/log.sh/$1"
-if [[ -d $a ]]; then
-        b="$(ls -1 "$a" | sed 's/^\([1-9][0-9]*\)\( .*\)\?/\1/' | sort -n | tail -n1)"
-fi
-((++b))
-c="$a/$b"
-if [[ ! -z ${@:2} ]]; then
-        c="$c ${@:2}"
-fi
-echo -n | install -D /dev/stdin "$c"
+install -d "$HOME/logger.sh"
 {
-        {
-                {
-                        {
-                                {
-                                        stdbuf -o0 -e0 "$@" 3>&- | stdbuf -i0 -o0 -e0 tee /dev/stderr 2>&3 3>&-
-                                } 2>&1 >&4 4>&- | stdbuf -i0 -o0 -e0 tee /dev/stderr 2>&3 3>&- 4>&-
-                        } >&5 5>&-
-                } 4>&1 | ./a.sh stdout >> "$c" 2>&3 3>&- 4>&- 5>&-
-        } 5>&1 | ./a.sh stderr >> "$c" 2>&3 3>&- 4>&- 5>&-
-} 3>&2
+if [[ -e $HOME/logger.sh/syslog ]]
+then exec 3>"$HOME/logger.sh/syslog.$(($(ls -1 "$HOME/logger.sh"|grep '^syslog\.[1-9][0-9]*$'|sed 's/^syslog\.\([1-9][0-9]*\)$/\1/'|sort -n|tail -n1)+1))"
+else exec 3>"$HOME/logger.sh/syslog"
+fi
+} 4>"$HOME/logger.sh/lock"
+format(){
+while IFS= read -r x; do stdbuf -o0 printf "%s $HOSTNAME $1: %s\n" "$(stdbuf -o0 date '+%b %e %T')" "$x"; done
+}
+echo "$@"|format 0 >&3
+{
+{
+{
+{
+{
+{
+"$@" 4>&-|tee /dev/stderr 2>&4 4>&-
+} 2>&1 >&6|tee /dev/stderr 2>&5 4>&- 5>&- 6>&-
+}>&7
+} 6>&1|format 1 >&3 4>&- 5>&- 6>&- 7>&-
+} 7>&1|format 2 >&3 4>&- 5>&- 6>&- 7>&-
+} 4>&1
+} 5>&2
