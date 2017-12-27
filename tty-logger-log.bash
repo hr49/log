9c0c4f3a11aed559bab47eca95d7d3084b54233f
matthew <m5w@users.noreply.github.com>
2017-12-26 21:57:24 -0500

    Fix sources of renamed init script

--- tty-logger-log.bash
+++ tty-logger-log.bash
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-. tty-logger-init
+. tty-logger-init.bash
 
 _default_tty_log_exists="$({
 [[ -e $_default_tty_log_filename ]]
