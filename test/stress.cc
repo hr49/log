1eba956f8a4de0d857acae60d3834cadc755f209
matthew <m5w@users.noreply.github.com>
2018-08-11 04:06:31 -0400

    Make format string with numeric limits

--- test/stress.cc
+++ test/stress.cc
@@ -1,24 +1,31 @@
 #include <cstdio>
 
+#include <limits>
 #include <random>
+#include <string>
 
 int main() {
   std::random_device PseudorandomIntegerGeneratorSeeder;
-  std::mt19937 PseudorandomIntegerGenerator(
-      PseudorandomIntegerGeneratorSeeder());
-  std::uniform_int_distribution<uint_fast8_t> PseudorandomBooleanGenerator(0u,
-                                                                           1u);
+  std::mt19937 PseudorandomIntegerGenerator{
+      PseudorandomIntegerGeneratorSeeder()};
+  std::uniform_int_distribution<uint_fast8_t> PseudorandomBooleanGenerator{0u,
+                                                                           1u};
+  typedef size_t LineIndexType;
+  const auto Format{
+      "%" + std::to_string(std::numeric_limits<LineIndexType>::digits10) +
+      "zu\n"};
+  const auto FormatCStr{Format.c_str()};
 
   /*
    * `1'000'000` is inspired by the default of the same value of the argument
-   * `number` to Python's `timeit.timeit`.
+   * `number` to Python's `timeit.timeit` function.
    */
-  constexpr size_t NUMBER_OF_LINES = 1'000'000;
+  constexpr LineIndexType NUMBER_OF_LINES{1'000'000};
 
-  for (size_t LineIndex = 0u; LineIndex < NUMBER_OF_LINES; ++LineIndex) {
+  for (LineIndexType LineIndex{0u}; LineIndex < NUMBER_OF_LINES; ++LineIndex) {
     if (PseudorandomBooleanGenerator(PseudorandomIntegerGenerator))
-      std::fprintf(stderr, "%20zu\n", LineIndex);
+      std::fprintf(stderr, FormatCStr, LineIndex);
     else
-      std::fprintf(stdout, "%20zu\n", LineIndex);
+      std::fprintf(stdout, FormatCStr, LineIndex);
   }
 }
